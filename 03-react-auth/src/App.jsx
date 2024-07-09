c4bdcca5c3975a0d73738d399bbf062090f4a67a
VahagnCh <vahagnchavushyan@gmail.com>
2024-07-08 19:57:42 -0600

    Add AuthProvider to the App

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -2,14 +2,17 @@ import './App.css'
 import Header from './components/Header/Header'
 import { BrowserRouter } from 'react-router-dom'
 import RoutesIndex from '@/routes/Index'
+import { AuthProvider } from '@/context/AuthContext'
 
 function App () {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <RoutesIndex />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          <RoutesIndex />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
