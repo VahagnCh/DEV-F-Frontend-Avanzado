cbbd19964627f0e54792dbbd6b7fa1c12db4bde1
VahagnCh <vahagnchavushyan@gmail.com>
2024-07-08 19:17:16 -0600

    Login con API y Token

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Header from './components/Header/Header'
+import { BrowserRouter } from 'react-router-dom'
+import RoutesIndex from '@/routes/Index'
 
 function App () {
   return (
     <>
-      <Header />
-      <h1>React Auth</h1>
+      <BrowserRouter>
+        <Header />
+        <RoutesIndex />
+      </BrowserRouter>
     </>
   )
 }
--- 03-react-auth/src/pages/Login.jsx
+++ 03-react-auth/src/pages/Login.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { loginUserService } from '@/services/userServices'
+import '@/styles/form.css'
+import logo from '@/assets/react.svg'
 
 const Login = () => {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await loginUserService(data)
+      if (response.status === 200) {
+        navigate('/dashboard')
+        // Guardamos el token en el localStorage
+        window.localStorage.setItem('token', response.data.token)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <h1>Login</h1>
+    <main className='form-signin w-100 m-auto'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <img
+          className='mb-4'
+          src={logo}
+          alt=''
+          width={72}
+          height={57}
+        />
+        <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+        <div className='form-floating'>
+          <input
+            type='email'
+            className='form-control'
+            id='floatingInput'
+            placeholder='name@example.com'
+            {...register('email', { required: true })}
+          />
+          {errors.email && <span>This field is required</span>}
+          <label htmlFor='floatingInput'>Email address</label>
+        </div>
+        <div className='form-floating'>
+          <input
+            type='password'
+            className='form-control'
+            id='floatingPassword'
+            placeholder='Password'
+            {...register('password', { required: true })}
+          />
+          {errors.password && <span>This field is required</span>}
+          <label htmlFor='floatingPassword'>Password</label>
+        </div>
+
+        <button className='btn btn-primary w-100 py-2' type='submit'>
+          Sign in
+        </button>
+        <p className='mt-5 mb-3 text-body-secondary'>© 2017–2024</p>
+      </form>
+    </main>
+
   )
 }
-
 export default Login
